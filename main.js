097012edad0a46d0a08dd653f07ae0688d48f2a2
Nischal1020 <nischal.khadka@mail.bcu.ac.uk>
2024-08-26 19:00:33 +0545

    code file

---
+++ main.js
@@ -0,0 +1,7 @@
+const instagram = document.querySelector(".instagram__flex");
+
+Array.from(instagram.children).forEach((item) => {
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  instagram.appendChild(duplicateNode);
+});
